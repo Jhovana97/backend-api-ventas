d6c7062d4191be9a25d979e7aa09e56e5adda5b1
Jhovana <jhovanapatty95@gmail.com>
2026-04-26 23:47:13 -0400

    implementar uso de @PathVariable y @RequestBody en RolController junto a crear un RolRequest DTO

--- src/main/java/com/example/comprar_ventas/controller/RolController.java
+++ src/main/java/com/example/comprar_ventas/controller/RolController.java
@@ -2,10 +2,14 @@ package com.example.comprar_ventas.controller;
 
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.comprar_ventas.dto.RolRequest;
 
 @RestController
 @RequestMapping("/rol")
@@ -15,19 +19,20 @@ public class RolController {
     public String listarRoles() {
         return "Listado de roles";
     }
-    @GetMapping 
-    public String obtenerRolPorId(Integer rolId) {
+    @GetMapping("/{rolId}")// /rol/1 para obtener el rol con id 1
+    public String obtenerRolPorId(@PathVariable Integer rolId) {
         return "Rol con id: " + rolId;
     }
     @PostMapping
-    public String crearRol(Object rol) {
+    public String crearRol(@RequestBody  RolRequest rolRequest) {
         return "Rol creado";
     }
-    @PutMapping
-    public String actualizarRol(Object rol) {
+    @PutMapping("/{rolId}")
+    public String actualizarRol(@PathVariable Integer rolId, @RequestBody  RolRequest rolRequest) {
         return "Rol actualizado";
     }
-    @DeleteMapping String borrarRol(Integer rol){
+    @DeleteMapping("/{id}")
+    public String borrarRol(@PathVariable Integer id) {
         return "Rol eliminado";
     }
 }
---
+++ src/main/java/com/example/comprar_ventas/dto/RolRequest.java
@@ -0,0 +1,6 @@
+package com.example.comprar_ventas.dto;
+
+public class RolRequest {
+    private String nombre;
+    private String descripcion;
+}
