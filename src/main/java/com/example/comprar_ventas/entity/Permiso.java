a248cf583906145cb6f023ae5aa946cf95400395
Jhovana <jhovanapatty95@gmail.com>
2026-05-01 23:02:15 -0400

    se configuro la relacion @ManyToMany se creo la tabla intermedia rol_permiso, en las entdades Rol y Permiso

--- src/main/java/com/example/comprar_ventas/entity/Permiso.java
+++ src/main/java/com/example/comprar_ventas/entity/Permiso.java
@@ -1,11 +1,27 @@
 package com.example.comprar_ventas.entity;
 
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Permiso {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
 
+    @Column(nullable = false, length = 100)
     private String nombre;
 
+    @Column(length = 100)
     private String recurso;
-
+    
+    @Column(nullable = false, length = 100)
     private String accion;
+
+    private List<Rol> roles;
 }
--- src/main/java/com/example/comprar_ventas/entity/Rol.java
+++ src/main/java/com/example/comprar_ventas/entity/Rol.java
@@ -1,11 +1,17 @@
 package com.example.comprar_ventas.entity;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
 
 @Entity
 public class Rol {
@@ -14,8 +20,16 @@ public class Rol {
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Integer id;
 
-    @Column(nullable = false, unique = true)
-    private String nombre;
+    @Column(nullable = false, length = 100, name = "nombre")
+    private String nombreCompleto;
 
     private String descripcion;
+    
+    @ManyToMany
+    @JoinTable(
+        name = "rol permiso",
+        joinColumns = @JoinColumn(name = "rol_id"),
+        inverseJoinColumns = @JoinColumn(name = "permiso_id")
+    )
+    private List<Permiso> permisos;
 }
